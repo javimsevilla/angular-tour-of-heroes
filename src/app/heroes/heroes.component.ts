57e5481eb7eb95af2454f1772c7b168251d7c9e6
Javi Molina <jmolina@mysphera.com>
2021-08-12 19:05:25 +0200

    Add getHeroes()

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -18,4 +18,8 @@ export class HeroesComponent implements OnInit {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 }
